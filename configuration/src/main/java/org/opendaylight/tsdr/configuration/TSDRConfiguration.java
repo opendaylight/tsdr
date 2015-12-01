8f7b0a15f3385cedcb7952c1bee69e861d1eaf38
Sharon Aicler <saichler@cisco.com>
2015-12-01 06:56:48 -0800

    Convert configuration to RPC
    Change-Id: I9d467082765051f0512ce5ee33a37e05713c59bb
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

--- configuration/src/main/java/org/opendaylight/tsdr/configuration/TSDRConfiguration.java
+++ configuration/src/main/java/org/opendaylight/tsdr/configuration/TSDRConfiguration.java
@@ -7,12 +7,25 @@
  */
 package org.opendaylight.tsdr.configuration;
 
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.GetAllPropertiesOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.GetAllPropertiesOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.GetPropertyInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.GetPropertyOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.GetPropertyOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.TsdrConfigurationService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.getallproperties.output.Properties;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.configuration.rev151130.getallproperties.output.PropertiesBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.concurrent.Future;
 
 /**
  * Created by saichler on 11/30/15
@@ -22,19 +35,14 @@ import java.util.Hashtable;
  * The update method is also invoked whenever there is a change to the file to
  * reload the configuration @ runtime.
  */
-public class TSDRConfiguration implements ManagedService {
+public class TSDRConfiguration implements ManagedService,TsdrConfigurationService {
 
-    private static TSDRConfiguration instance = new TSDRConfiguration();
     private Dictionary<String, Object> configurations = new Hashtable<>();
 
     public static final String P_HOST = "host";
 
-    private TSDRConfiguration() {
+    public TSDRConfiguration() {
         this.configurations.put(P_HOST, "127.0.0.1");
-    }
-
-    public static TSDRConfiguration getInstance() {
-        return instance;
     }
 
     @Override
@@ -49,11 +57,31 @@ public class TSDRConfiguration implements ManagedService {
         }
     }
 
-    public Dictionary<String, Object> getConfiguration() {
-        return this.configurations;
+    private String getProperty(String name){
+        return (String)this.configurations.get(name);
     }
 
-    public Object getConfig(String name){
-        return this.configurations.get(name);
+    @Override
+    public Future<RpcResult<GetPropertyOutput>> getProperty(GetPropertyInput input) {
+        GetPropertyOutputBuilder b = new GetPropertyOutputBuilder();
+        String value = (String)configurations.get(input.getName());
+        b.setValue(value);
+        return RpcResultBuilder.success(b).buildFuture();
+    }
+
+    @Override
+    public Future<RpcResult<GetAllPropertiesOutput>> getAllProperties() {
+        GetAllPropertiesOutputBuilder output = new GetAllPropertiesOutputBuilder();
+        List<Properties> result = new ArrayList<>(configurations.size());
+        Enumeration<String> keys = configurations.keys();
+        while(keys.hasMoreElements()){
+            String key = keys.nextElement();
+            PropertiesBuilder b = new PropertiesBuilder();
+            b.setKey(key);
+            b.setValue((String)configurations.get(key));
+            result.add(b.build());
+        }
+        output.setProperties(result);
+        return RpcResultBuilder.success(output).buildFuture();
     }
 }
