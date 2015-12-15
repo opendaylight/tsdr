351e775b91c35ff06f0ab3bbc48511305a13416e
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-15 17:29:39 -0500

    Add missing license headers
    Change-Id: I8839662820c31e4b413de9122276dc23cb89c97c
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- collectors/controller-metrics-collector/src/main/java/org/opendaylight/controller/config/yang/config/tsdr/controller/metrics/collector/TSDRCMCModule.java
+++ collectors/controller-metrics-collector/src/main/java/org/opendaylight/controller/config/yang/config/tsdr/controller/metrics/collector/TSDRCMCModule.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.controller.config.yang.config.tsdr.controller.metrics.collector;
 
 import org.opendaylight.tsdr.collectors.cmc.ControllerMetricCollector;
@@ -5,7 +13,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controll
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TSDRCMCModule extends org.opendaylight.controller.config.yang.config.tsdr.controller.metrics.collector.AbstractTSDRCMCModule 
+public class TSDRCMCModule extends org.opendaylight.controller.config.yang.config.tsdr.controller.metrics.collector.AbstractTSDRCMCModule
         implements AutoCloseable{
 
     private static final Logger logger = LoggerFactory.getLogger(TSDRCMCModule.class);
--- collectors/netflow-statistics-collector/src/main/java/org/opendaylight/controller/config/yang/config/tsdr/netflow/statistics/collector/TSDRNetFlowCollectorModuleFactory.java
+++ collectors/netflow-statistics-collector/src/main/java/org/opendaylight/controller/config/yang/config/tsdr/netflow/statistics/collector/TSDRNetFlowCollectorModuleFactory.java
@@ -1,4 +1,13 @@
 /*
+ * Copyright (c) 2015 Dell Inc. and others.  All rights reserved.
+ * Copyright (c) 2015 xFlow Research Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/*
 * Generated file
 *
 * Generated from: yang module name: tsdr-netflow-statistics-collector yang module local name: tsdr-netflow-statistics-collector
