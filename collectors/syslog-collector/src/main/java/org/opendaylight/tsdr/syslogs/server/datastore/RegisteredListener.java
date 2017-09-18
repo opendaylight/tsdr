009e7c8c2d6441f5bd15000c44ed0827c73d48d3
Tom Pantelis <tompantelis@gmail.com>
2017-09-18 15:12:16 -0400

    Convert DataChangeListeners to DataTreeChangeListeners
    The DataChangeListener API has been deprecated for a couple releases
    with removal in Oxygen or Flourine.
    
    Change-Id: I8ae2eac5b8deaf2eae005c691cccd77d1bc1922b
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- collectors/syslog-collector/src/main/java/org/opendaylight/tsdr/syslogs/server/datastore/RegisteredListener.java
+++ collectors/syslog-collector/src/main/java/org/opendaylight/tsdr/syslogs/server/datastore/RegisteredListener.java
@@ -9,28 +9,27 @@
 
 package org.opendaylight.tsdr.syslogs.server.datastore;
 
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.SyslogDispatcher;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.syslog.dispatcher.SyslogListener;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.syslog.dispatcher.SyslogListenerKey;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.binding.DataObject;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Collection;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.SyslogDispatcher;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.syslog.dispatcher.SyslogListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.syslog.dispatcher.SyslogListenerKey;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This RegisteredListener handles the listening and notification
@@ -39,14 +38,12 @@ import java.net.URLConnection;
  * @author Wei Lai(weilai@tethrnet.com)
  */
 
-public class RegisteredListener implements DataChangeListener {
-    private DataBroker db;
+public class RegisteredListener implements DataTreeChangeListener<SyslogListener> {
+    private final DataBroker db;
     private final Logger LOG = LoggerFactory.getLogger(RegisteredListener.class);
-    private String listenerId;
-    private String callBackUrl;
-    private ListenerRegistration<DataChangeListener> listener;
-    private OutputStreamWriter out;
-    private BufferedReader responseReader;
+    private final String listenerId;
+    private final String callBackUrl;
+    private ListenerRegistration<?> listenerReg;
 
     public RegisteredListener(DataBroker db, String listenerId, String url) {
         this.db = db;
@@ -54,7 +51,7 @@ public class RegisteredListener implements DataChangeListener {
         this.callBackUrl = url;
     }
 
-    private InstanceIdentifier<SyslogListener> toInstanceIdentifier(String listenerId) {
+    private InstanceIdentifier<SyslogListener> toInstanceIdentifier() {
         InstanceIdentifier<SyslogListener> iid = InstanceIdentifier.create(SyslogDispatcher.class)
                 .child(SyslogListener.class, new SyslogListenerKey(listenerId));
         return iid;
@@ -64,9 +61,9 @@ public class RegisteredListener implements DataChangeListener {
      * monitor the listener node in operational tree
      */
     public void listen() {
-        InstanceIdentifier<SyslogListener> iid = this.toInstanceIdentifier(this.listenerId);
-        this.listener = db.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL, iid,
-                this, AsyncDataBroker.DataChangeScope.SUBTREE);
+        InstanceIdentifier<SyslogListener> iid = this.toInstanceIdentifier();
+        this.listenerReg = db.registerDataTreeChangeListener(new DataTreeIdentifier<>(
+                LogicalDatastoreType.OPERATIONAL, iid), this);
     }
 
     /**
@@ -74,7 +71,7 @@ public class RegisteredListener implements DataChangeListener {
      */
     public boolean close() {
         try {
-            this.listener.close();
+            this.listenerReg.close();
         } catch (Exception e) {
             LOG.error("unable to close listener");
             return false;
@@ -86,45 +83,53 @@ public class RegisteredListener implements DataChangeListener {
      * generate notification when Syslog message match certain filter and sent it to the corresponding client
      */
     @Override
-    public void onDataChanged(AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
-        InstanceIdentifier<SyslogListener> iid = this.toInstanceIdentifier(this.listenerId);
-        SyslogListener listener = (SyslogListener) change.getUpdatedData().get(iid);
-        if (listener != null) {
-            LOG.info("  get updated message from: " + listener.getListenerId());
-            LOG.info("  the updated message is: " + listener.getSyslogMessage());
+    public void onDataTreeChanged(Collection<DataTreeModification<SyslogListener>> changes) {
+        for (DataTreeModification<SyslogListener> change: changes) {
+            DataObjectModification<SyslogListener> rootNode = change.getRootNode();
+            switch (rootNode.getModificationType()) {
+                case WRITE:
+                case SUBTREE_MODIFIED:
+                    if (this.callBackUrl != null) {
+                        final SyslogListener listener = rootNode.getDataAfter();
+                        LOG.info("Got updated message from {}: {} ", listener.getListenerId(),
+                                listener.getSyslogMessage());
 
-            if (this.callBackUrl != null) {
-                try {
-                    URL url = new URL(callBackUrl);
-                    URLConnection urlConnection = url.openConnection();
-                    urlConnection.setDoOutput(true);
-                    urlConnection.setDoInput(true);
-                    urlConnection.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+                        OutputStreamWriter out = null;
+                        try {
+                            URL url = new URL(callBackUrl);
+                            URLConnection urlConnection = url.openConnection();
+                            urlConnection.setDoOutput(true);
+                            urlConnection.setDoInput(true);
+                            urlConnection.setRequestProperty("content-type", "application/x-www-form-urlencoded");
 
-                    out = new OutputStreamWriter(urlConnection.getOutputStream());
-                    out.write("received updated message " + listener.getSyslogMessage());
-                    out.flush();
+                            out = new OutputStreamWriter(urlConnection.getOutputStream());
+                            out.write("received updated message " + listener.getSyslogMessage());
+                            out.flush();
 
-                    int responseCode = ((HttpURLConnection) urlConnection).getResponseCode();
-                    if (HttpURLConnection.HTTP_OK == responseCode) {
-                        String readLine;
-                        responseReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                        while ((readLine = responseReader.readLine()) != null) {
-                            LOG.info("the updated message is: " + readLine);
+                            int responseCode = ((HttpURLConnection) urlConnection).getResponseCode();
+                            if (HttpURLConnection.HTTP_OK == responseCode) {
+                                String readLine;
+                                try (BufferedReader responseReader = new BufferedReader(
+                                        new InputStreamReader(urlConnection.getInputStream()))) {
+                                    while ((readLine = responseReader.readLine()) != null) {
+                                        LOG.info("The updated message response is: " + readLine);
+                                    }
+                                }
+                            }
+                        } catch (IOException e) {
+                            LOG.error("Error processing message", e);
+                        } finally {
+                            try {
+                                out.close();
+                            } catch (IOException e) {
+                                LOG.error("unable to close the stream");
+                            }
                         }
                     }
-                } catch (IOException e) {
-                    LOG.error("connection refused");
-                } finally {
-                    try {
-                        out.close();
-                        responseReader.close();
-                    } catch (IOException e) {
-                        LOG.error("unable to close the stream");
-                    }
-                }
+                    break;
+                default:
+                    break;
             }
-
         }
     }
 }
--- collectors/syslog-collector/src/test/java/org/opendaylight/tsdr/syslogs/RegisteredListenerTest.java
+++ collectors/syslog-collector/src/test/java/org/opendaylight/tsdr/syslogs/RegisteredListenerTest.java
@@ -9,87 +9,64 @@
 
 package org.opendaylight.tsdr.syslogs;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+import java.util.Collections;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.tsdr.syslogs.server.datastore.RegisteredListener;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.RegisterFilterInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.RegisterFilterInputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.RegisterFilterOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.SyslogDispatcher;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.syslog.dispatcher.SyslogListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.syslog.dispatcher.SyslogListenerBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.syslog.collector.rev151007.syslog.dispatcher.SyslogListenerKey;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.RpcResult;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.*;
 
 /**
  * Test for RegisteredListener.
  * @author Wei Lai(weilai@tethrnet.com)
  */
 public class RegisteredListenerTest {
-    private DataBroker dataBroker = mock(DataBroker.class);
-    private RegisteredListener registeredListener = new RegisteredListener(dataBroker,"123","http://localhost:9001/server");
-    private RegisteredListener registeredListener2 = new RegisteredListener(dataBroker,"123",null);
+    private final DataBroker dataBroker = mock(DataBroker.class);
 
-    private ListenerRegistration<DataChangeListener> listener = mock(ListenerRegistration.class);
+    private final ListenerRegistration<?> listener = mock(ListenerRegistration.class);
     InstanceIdentifier<SyslogListener> iid = InstanceIdentifier.create(SyslogDispatcher.class)
             .child(SyslogListener.class, new SyslogListenerKey("123"));
-    private AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change = mock(AsyncDataChangeEvent.class);
-    private OutputStreamWriter out = mock(OutputStreamWriter.class);
-    private URLConnection urlConnection = mock(URLConnection.class);
-    private OutputStream outputStram = mock(OutputStream.class );
-
-
-
 
     @Before
     public void mockSetUp() {
-
     }
 
     @Test
     public void testCloseWithException() {
-        when(dataBroker.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL, iid,
-                registeredListener, AsyncDataBroker.DataChangeScope.SUBTREE)).thenReturn(listener);
+        RegisteredListener registeredListener = new RegisteredListener(dataBroker,"123","http://localhost:9001/server");
+
+        doReturn(listener).when(dataBroker).registerDataTreeChangeListener(new DataTreeIdentifier<>(
+                LogicalDatastoreType.OPERATIONAL, iid), registeredListener);
         RuntimeException closeException = mock(RuntimeException.class);
         doThrow(closeException).when(listener).close();
 
         registeredListener.listen();
 
         Assert.assertEquals(false,registeredListener.close());
-
     }
-
 
     @Test
     public void testCloseSuccessfully() {
+        RegisteredListener registeredListener = new RegisteredListener(dataBroker,"123","http://localhost:9001/server");
 
-        when(dataBroker.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL, iid,
-                registeredListener, AsyncDataBroker.DataChangeScope.SUBTREE)).thenReturn(listener);
+        doReturn(listener).when(dataBroker).registerDataTreeChangeListener(new DataTreeIdentifier<>(
+                LogicalDatastoreType.OPERATIONAL, iid), registeredListener);
 
         doNothing().when(listener).close();
 
@@ -97,25 +74,26 @@ public class RegisteredListenerTest {
         Assert.assertEquals(true,registeredListener.close());
     }
 
+    @SuppressWarnings("unchecked")
     @Test
-    public void testOndataChangedWithNoCallbcakURL() throws IOException {
+    public void testOndataChangedWithNoCallbackURL() throws IOException {
+        RegisteredListener registeredListener = new RegisteredListener(dataBroker,"123", null);
 
-        when(dataBroker.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL, iid,
-                registeredListener, AsyncDataBroker.DataChangeScope.SUBTREE)).thenReturn(listener);
+        doReturn(listener).when(dataBroker).registerDataTreeChangeListener(new DataTreeIdentifier<>(
+                LogicalDatastoreType.OPERATIONAL, iid), registeredListener);
+
+        DataTreeModification<SyslogListener> mockDataTreeModification = mock(DataTreeModification.class);
+        DataObjectModification<SyslogListener> mockModification = mock(DataObjectModification.class);
+        doReturn(mockModification).when(mockDataTreeModification).getRootNode();
+
         SyslogListener syslogListener = new SyslogListenerBuilder()
                 .setListenerId("321")
                 .setSyslogMessage("cisco")
                 .build();
-        Map<InstanceIdentifier<?>,DataObject> map = new HashMap<>();
-        map.put(iid,syslogListener);
-        when(change.getUpdatedData()).thenReturn(map);
 
-        registeredListener2.onDataChanged(change);
-        Assert.assertNotNull(syslogListener);
+        doReturn(syslogListener).when(mockModification).getDataAfter();
+        doReturn(DataObjectModification.ModificationType.WRITE).when(mockModification).getModificationType();
+
+        registeredListener.onDataTreeChanged(Collections.singleton(mockDataTreeModification));
     }
-
-
-
-
-
 }
