1034adbe8773cc5de63bbe089e26be67e258fff1
Tom Pantelis <tompantelis@gmail.com>
2018-09-17 12:57:53 -0400

    Add AbstractBatchingLogCollector
    Extracted from BatchingLogCollector - genericizes the type of
    the incoming objects that are queued and transforms them to
    TSDRLogRecords via an abstract method. BatchingLogCollector
    specifies TSDRLogRecord as the incoming type.
    
    Change-Id: Ib255c0c9cf7c800c396dd470844c37ac9e7fadd6
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- collectors/collector-spi/src/main/java/org/opendaylight/tsdr/collector/spi/logger/BatchingLogCollector.java
+++ collectors/collector-spi/src/main/java/org/opendaylight/tsdr/collector/spi/logger/BatchingLogCollector.java
@@ -8,23 +8,13 @@
 
 package org.opendaylight.tsdr.collector.spi.logger;
 
-import java.util.ArrayDeque;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ScheduledFuture;
-import java.util.stream.Collectors;
-import javax.annotation.PreDestroy;
-import javax.annotation.concurrent.GuardedBy;
-
-import org.opendaylight.tsdr.collector.spi.RPCFutures;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.opendaylight.tsdr.spi.scheduler.SchedulerService;
 import org.opendaylight.yang.gen.v1.opendaylight.tsdr.rev150219.DataCategory;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.InsertTSDRLogRecordInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.TsdrCollectorSpiService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.inserttsdrlogrecord.input.TSDRLogRecord;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.inserttsdrlogrecord.input.TSDRLogRecordBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 /**
@@ -36,120 +26,45 @@ import org.slf4j.LoggerFactory;
  * @author Matthieu Cauffiez
  */
 
-public class BatchingLogCollector implements AutoCloseable {
-    /**
-     * the logger of the class.
+public class BatchingLogCollector extends AbstractBatchingLogCollector<TSDRLogRecord> {
+    /*
+     * The default interval after which the data is persisted, specified in milli-seconds.
      */
-    private static final Logger LOG = LoggerFactory.getLogger(BatchingLogCollector.class);
-    /**
-     * default interval after which the data is persisted, specified in milli-seconds.
-     */
-    private static final long DEFAULT_CHECK_INTERVAL_IN_MILLISECONDS = 5000;
+    private static final long DEFAULT_FLUSH_INTERVAL_IN_MILLISECONDS = 5000;
 
     /**
-     * a reference to the collector SPI service.
+     * The index of the current record, useful to distinguish records that are received in the same millisecond.
+     * It is incremented for each record received, and then zeroed again after the data is persisted.
      */
-    private final TsdrCollectorSpiService tsdrCollectorSpiService;
-
-    private final ScheduledFuture<?> future;
-
-    /**
-     * The queue in which the data is cached before persisting.
-     */
-    @GuardedBy("queueMutex")
-    private final ArrayDeque<TSDRLogRecord> queue = new ArrayDeque<>();
-
-    /**
-     * A mutex used for locking the queue to prevent race conditions between multiple threads.
-     */
-    private final Object queueMutex = new Object();
-
-    private final String collectorName;
-
-    /**
-     * The index of the current record, useful to distinguish records are received in the same milli-second
-     * it is incremented for each record received, and then zeroed again after the data is persisted.
-     */
-    @GuardedBy("queueMutex")
-    private int currentIndex = 0;
+    private final AtomicInteger currentIndex = new AtomicInteger();
 
 
     public BatchingLogCollector(TsdrCollectorSpiService tsdrCollectorSpiService, SchedulerService schedulerService,
-                                String collectorName, long initialDelay, long retryInterval) {
-        this.tsdrCollectorSpiService = tsdrCollectorSpiService;
-        this.collectorName = collectorName;
-        this.future = schedulerService.scheduleTaskAtFixedRate(this::processQueue, initialDelay, retryInterval);
-        LOG.info("{} logger initialized", collectorName);
+                                String collectorName, long storeFlushInterval) {
+        super(tsdrCollectorSpiService, schedulerService, collectorName, storeFlushInterval);
     }
 
     public BatchingLogCollector(TsdrCollectorSpiService tsdrCollectorSpiService, SchedulerService schedulerService,
                                 String collectorName) {
-        this(tsdrCollectorSpiService, schedulerService, collectorName,
-                DEFAULT_CHECK_INTERVAL_IN_MILLISECONDS, DEFAULT_CHECK_INTERVAL_IN_MILLISECONDS);
+        this(tsdrCollectorSpiService, schedulerService, collectorName, DEFAULT_FLUSH_INTERVAL_IN_MILLISECONDS);
     }
 
     /**
-     * persists the cache queue.
-     * @param records the queue to persist
-     */
-    private void store(List<TSDRLogRecord> records) {
-        InsertTSDRLogRecordInputBuilder input = new InsertTSDRLogRecordInputBuilder();
-        input.setTSDRLogRecord(records);
-        input.setCollectorCodeName(collectorName);
-
-        RPCFutures.logResult(tsdrCollectorSpiService.insertTSDRLogRecord(input.build()), "insertTSDRLogRecord", LOG);
-    }
-
-    /**
-     * It copies the cached queue into another queue, then clears the queue and the currentIndex.
-     * Then, it checks if that new queue has data, if it has data, it gets persisted.
-     * Finally, it checks if the module has shut down. If it has, it cancels the timer task.
-     * The reason behind using a temporary queue is to free the original queue as quickly as possible, so that if
-     * insertLog is waiting, it could resume its execution as soon as possible
-     */
-    private void processQueue() {
-        final List<TSDRLogRecord> tempQueue;
-        synchronized (queueMutex) {
-            if (!queue.isEmpty()) {
-                tempQueue = queue.stream().collect(Collectors.toList());
-                queue.clear();
-                currentIndex = 0;
-            } else {
-                tempQueue = Collections.emptyList();
-            }
-        }
-        if (!tempQueue.isEmpty()) {
-            store(tempQueue);
-        }
-    }
-
-    /**
-     * builds a log from the provided parameters, and inserts it into the cache queue.
+     * Builds a log from the provided parameters, and inserts it into the cache queue.
+     *
      * @param elementID The ID of the element.
      * @param timeStamp time when the log was produced.
      * @param data Content of the log.
      * @param dataCategory type of data.
      */
     public void insertLog(String elementID, long timeStamp, String data, DataCategory dataCategory) {
-
-        TSDRLogRecordBuilder recordBuilder = new TSDRLogRecordBuilder();
-
-        recordBuilder.setNodeID(elementID);
-        recordBuilder.setTimeStamp(timeStamp);
-        recordBuilder.setRecordFullText(data);
-        recordBuilder.setTSDRDataCategory(dataCategory);
-
-        synchronized (queueMutex) {
-            recordBuilder.setIndex(currentIndex);
-            currentIndex++;
-            queue.add(recordBuilder.build());
-        }
+        enqueue(new TSDRLogRecordBuilder().setNodeID(elementID).setTimeStamp(timeStamp).setRecordFullText(data)
+                .setTSDRDataCategory(dataCategory).setIndex(currentIndex.getAndIncrement()).build());
     }
 
     @Override
-    @PreDestroy
-    public void close() {
-        this.future.cancel(false);
+    protected List<TSDRLogRecord> transform(List<TSDRLogRecord> from) {
+        currentIndex.set(0);
+        return from;
     }
-
 }
