6717a19c9c8caf79e49588875beec5d9658e1b5e
Tom Pantelis <tompantelis@gmail.com>
2018-09-22 10:34:33 -0400

    Simplify SyslogFilter
    The interface was simplified to just the matches(...) API. The
    getPersistenceDestination API was not used and not clearly defined,
    specifically as to what the return int value signifies. For the
    filterAndParseSyslog API, it seems the "filter" aspect should already
    be handled by matches - it also seems that building the TSDRLogRecord
    shouldn't be related to the filtering and should be consistent. There is
    only one implementation of SyslogFilter that basically is a no-op so it's
    hard to know what the original intention was.
    
    Also, the map in the SyslogFilterManager was changed from SyslogFilter[] to
    SyslogFilter. It's unclear why this was an array - the code only expected one
    element and, even if it allowed multiple, the applyFilters method only returns
    a single TSDRLogRecord. So again, unclear as to what the original intention was.
    
    Change-Id: I8264056f0bb48d3edbbf6b918b0dcfbccffd6107
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- collectors/syslog-collector/src/main/java/org/opendaylight/tsdr/syslogs/filters/SyslogFilter.java
+++ collectors/syslog-collector/src/main/java/org/opendaylight/tsdr/syslogs/filters/SyslogFilter.java
@@ -7,35 +7,19 @@
  */
 package org.opendaylight.tsdr.syslogs.filters;
 
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.inserttsdrlogrecord.input.TSDRLogRecord;
-
 /**
  * A syslog filter.
  *
  * @author Sharon Aicler(saichler@gmail.com)
  */
+@FunctionalInterface
 public interface SyslogFilter {
     /**
-     * Returns true if this syslog is compatible with this filter and this filter should be applied to it.
-     */
-    boolean match(String syslog);
-
-    /**
-     * if "match" then parse the syslog while filtering it.
-     * If null is returned then the syslog is dropped.
-     * Sometimes there is a syslog forwarder and this is its address. The originator address is in the syslog body,
-     * so we might want to filter according to that.
+     * Returns true if the given syslog parameters are compatible with this filter.
      *
      * @param syslog The syslog plain text
-     * @param forwarderAddress the datagram packet source address
-     * @param originatorAddress the datagram packet source address
-     * @return TSDRLogRecord
+     * @param packetSourceAddress the syslog's packet source address
+     * @param originatorAddress the syslog's originator address
      */
-    TSDRLogRecord filterAndParseSyslog(String syslog,String forwarderAddress,String originatorAddress);
-
-    /**
-     * Determinate the persistence destination of the syslog. Currently we have only one, we might have multiple in
-     * the future.
-     */
-    int getPersistenceDestination(TSDRLogRecord logRecord);
+    boolean matches(String syslog, String packetSourceAddress, String originatorAddress);
 }
--- collectors/syslog-collector/src/main/java/org/opendaylight/tsdr/syslogs/filters/SyslogFilterManager.java
+++ collectors/syslog-collector/src/main/java/org/opendaylight/tsdr/syslogs/filters/SyslogFilterManager.java
@@ -7,10 +7,15 @@
  */
 package org.opendaylight.tsdr.syslogs.filters;
 
-import java.util.HashMap;
+import com.google.common.collect.ImmutableMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.opendaylight.tsdr.syslogs.server.decoder.Message;
+import org.opendaylight.yang.gen.v1.opendaylight.tsdr.rev150219.DataCategory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.inserttsdrlogrecord.input.TSDRLogRecord;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.inserttsdrlogrecord.input.TSDRLogRecordBuilder;
 
 /**
  * Manages syslog filters.
@@ -18,32 +23,34 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controll
  * @author Sharon Aicler(saichler@gmail.com)
  **/
 public class SyslogFilterManager {
-    private final Map<String, SyslogFilter[]> filters = new HashMap<>();
+    private static final SyslogFilter WILDCARD_FILTER = (syslog, packetSourceAddress, originatorAddress) -> true;
 
-    public SyslogFilterManager() {
-        filters.put("*",new SyslogFilter[]{new PersistAllSyslogFilter()});
-    }
+    private final AtomicInteger index = new AtomicInteger();
+    private final Map<String, SyslogFilter> filters = ImmutableMap.of();
 
-    public TSDRLogRecord applyFilters(Message message) {
-        if (message != null) {
-            String syslogString = message.getContent().trim();
-            String packetHostAddress = message.getHostname();
-            String syslogOriginator = null;
-            int index1 = syslogString.indexOf("Original Address");
+    @Nullable
+    public TSDRLogRecord applyFilters(@Nonnull Message message) {
+        String syslogString = message.getContent().trim();
+        String packetHostAddress = message.getHostname();
+
+        String syslogOriginator = packetHostAddress;
+        int index1 = syslogString.indexOf("Original Address");
+        if (index1 != -1) {
             int index2 = syslogString.indexOf("=", index1);
-            int index3 = syslogString.indexOf(" ", index2 + 2);
-            if (index1 != -1 && index2 != -1 && index3 != -1 && index2 > index1 && index3 > index2) {
-                syslogOriginator = syslogString.substring(index2 + 1, index3).trim();
-            } else {
-                syslogOriginator = packetHostAddress;
+            if (index2 != -1)  {
+                int index3 = syslogString.indexOf(" ", index2 + 2);
+                if (index3 != -1) {
+                    syslogOriginator = syslogString.substring(index2 + 1, index3).trim();
+                }
             }
-            SyslogFilter[] flt = filters.get(syslogOriginator);
-            if (flt == null) {
-                flt = filters.get("*");
-            }
-            if (flt.length == 1 && flt[0].match(syslogString)) {
-                return flt[0].filterAndParseSyslog(syslogString, packetHostAddress, syslogOriginator);
-            }
+        }
+
+        SyslogFilter filter = filters.getOrDefault(syslogOriginator, WILDCARD_FILTER);
+
+        if (filter.matches(syslogString, packetHostAddress, syslogOriginator)) {
+            return new TSDRLogRecordBuilder().setNodeID(syslogOriginator).setRecordFullText(syslogString)
+                .setTSDRDataCategory(DataCategory.SYSLOG).setTimeStamp(System.currentTimeMillis())
+                .setIndex(index.getAndIncrement()).build();
         }
 
         return null;
