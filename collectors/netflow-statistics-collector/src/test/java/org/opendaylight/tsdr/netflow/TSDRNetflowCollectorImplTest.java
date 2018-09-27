fa6000ce3e8fd03ac2b01ad669a7d7ffdcd9cd5e
Tom Pantelis <tompantelis@gmail.com>
2018-09-26 23:02:30 -0400

    Improve NetflowPacketParser
    Separated out v5 and v9 NetflowPacketParser classes
    for clarity with NetflowPacketParser as the abstract base.
    
    Instead of using hard-coded offsets into the byte stream
    which is error prone, the current position is tracked and
    updated automatically.
    
    The v5 code was mostly correct except the sampling_interval field
    is 14 bits and not 6.
    
    The v9 code was incorrect for several reasons wrt
    https://www.ietf.org/rfc/rfc3954.txt, in particular:
    
     - It assumed there is only one template when parsing a data flow set.
    
     - If there was no template, it assumed a particular template format.
       A data flow set cannot be parsed without a corresponding template.
    
     - When parsing a template, the template ID and field count were not
       parsed and thus were interpreted as a field record.
    
    The code also assumes a template flow set is always in the same packet
    as the data flow sets but templates are mostly sent once and are supposed
    to be cached in the collector to be used to decode subsequent data flow
    set packets. This will be addressed in another patch.
    
    Change-Id: I74c0f3bdae266418b34195e25aa35167fb43a5df
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- collectors/netflow-statistics-collector/src/test/java/org/opendaylight/tsdr/netflow/TSDRNetflowCollectorImplTest.java
+++ collectors/netflow-statistics-collector/src/test/java/org/opendaylight/tsdr/netflow/TSDRNetflowCollectorImplTest.java
@@ -24,7 +24,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.opendaylight.yang.gen.v1.opendaylight.tsdr.log.data.rev160325.tsdrlog.RecordAttributes;
+import org.opendaylight.yang.gen.v1.opendaylight.tsdr.rev150219.DataCategory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.InsertTSDRLogRecordInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.InsertTSDRLogRecordOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.tsdr.collector.spi.rev150915.TsdrCollectorSpiService;
@@ -35,8 +35,11 @@ import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
  * Unit test for Netflow statistics collector.
  *
  * @author Muhammad Umair(muhammad.umair@xflowresearch.com)
+ * @author Thomas Pantelis
 */
 public class TSDRNetflowCollectorImplTest {
+    private static final String SOURCE_IP = "127.0.0.1";
+
     private final TsdrCollectorSpiService mockSpiService = Mockito.mock(TsdrCollectorSpiService.class);
     private final TSDRNetflowCollectorImpl implObj = new TSDRNetflowCollectorImpl(mockSpiService);
     private DatagramSocket socket;
@@ -55,17 +58,8 @@ public class TSDRNetflowCollectorImplTest {
     }
 
     public void sendNetflowData(byte[] data) throws IOException {
-        DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("127.0.0.1"), 2055);
+        DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(SOURCE_IP), 2055);
         socket.send(packet);
-    }
-
-    public static byte[] hexStringToByteArray(String str) {
-        int len = str.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len - 1; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(str.charAt(i), 16) << 4) + Character.digit(str.charAt(i + 1), 16));
-        }
-        return data;
     }
 
     @Test
@@ -76,62 +70,32 @@ public class TSDRNetflowCollectorImplTest {
             return RpcResultBuilder.success(new InsertTSDRLogRecordOutputBuilder().build()).buildFuture();
         }).when(mockSpiService).insertTSDRLogRecord(any());
 
-        // netflow v5 data
-        byte[] data = hexStringToByteArray(
-                "0005000101bc454657a03ec60000000000000046000000000a0000020a0000030000000000030005"
-                + "000000010000004001bb5ae601bc4546109200500000110100020003201f0000");
-        sendNetflowData(data);
-
-        // netflow v9 packet
-        data = hexStringToByteArray(
-                "000900124a3d1d5857961622000168b700000000010003644a3cb4984a3cb498000000490000000100090000c0a80110080"
-                + "8080811009062003500000000000000181000000000004a3cb9f84a3cb9f80000003b00000001000a0000ac10047173ba"
-                + "bc031100cd20003500000000000000161000000000004a3cbcc84a3cba140000130200000007000a0002ac1004716e5dc"
-                + "2261100cd2101bb000073bab0ad00161000000000004a3cbf884a3cbf880000002800000001000a0002ac1004714ad005"
-                + "060600e46103e1000073bab0ad00161000000000004a3cc1684a3cc1680000002900000001000a0002ac1004716e5dc23"
-                + "90600e49101bb000073bab0ad00161000000000004a3cc6844a3cc6840000004500000001000a0000ac10047173babc03"
-                + "1100e448003500000000000000161000000000004a3cc8504a3cc8500000004b00000001000a0000ac10047173babc031"
-                + "100fed0003500000000000000161000000000004a3ccc684a3cc684000000ea00000003000a0000ac100471ac1007ff11"
-                + "000089008900000000000016161000000000004a3ccc344a3cc68c00000c7600000009000a0002ac1004716e5dc20f110"
-                + "0fcf001bb000073bab0ad00161000000000004a3ccd204a3ccd200000003c0000000100090000c0a8011f73babc031100"
-                + "ba09003500000000000000181000000000004a3ccd244a3ccd240000003c0000000100090000c0a8011f73babc0311008"
-                + "6aa003500000000000000181000000000004a3ccd284a3ccd28000000320000000100090000c0a8011f73babc031100b1"
-                + "fb003500000000000000181000000000004a3ccd284a3ccd28000000320000000100090000c0a8011f73babc031100db8"
-                + "d003500000000000000181000000000004a3d06704a3ccbf40000007900000003000a0002ac1004716e5dc21e0600e48b"
-                + "01bb000073bab0ad00161100000000004a3cd3b04a3cd3b00000002900000001000a0002ac1004716e5dc21e0600e4920"
-                + "1bb000073bab0ad00161000000000004a3cdc304a3cd9a40000057600000003000a0002ac1004714137df210600e4509c"
-                + "59000073bab0ad00161800000000004a3ce1c84a3cde900000013200000003000a0002ac10047189744bf40600e0d701b"
-                + "b000073bab0ad00161800000000004a3ce2a44a3ce2a40000005900000001000a0002ac1004719d3890d71100ec390dd8"
-                + "000073bab0ad0016100000000000");
-        sendNetflowData(data);
+        sendNetflowData(generateV5Data());
+        sendNetflowData(generateV5Data());
 
         Awaitility.await().atMost(10, TimeUnit.SECONDS).until(() -> {
-            return storedRecords.size() == 19;
+            return storedRecords.size() == 2;
         });
 
-        List<RecordAttributes> recordAttributes = storedRecords.get(0).getRecordAttributes();
-        assertEquals(27, recordAttributes.size());
-        assertEquals("version", recordAttributes.get(0).getName());
-        assertEquals("5", recordAttributes.get(0).getValue());
-        assertEquals("flowDuration", recordAttributes.get(26).getName());
-        assertEquals("60000", recordAttributes.get(26).getValue());
+        assertEquals(27, storedRecords.get(0).getRecordAttributes().size());
+        assertEquals("version", storedRecords.get(0).getRecordAttributes().get(0).getName());
+        assertEquals("5", storedRecords.get(0).getRecordAttributes().get(0).getValue());
+        assertEquals(SOURCE_IP, storedRecords.get(0).getNodeID());
+        assertEquals(Integer.valueOf(1), storedRecords.get(0).getIndex());
+        assertEquals(DataCategory.NETFLOW, storedRecords.get(0).getTSDRDataCategory());
 
-        recordAttributes = storedRecords.get(1).getRecordAttributes();
-        assertEquals(26, recordAttributes.size());
-        assertEquals("version", recordAttributes.get(0).getName());
-        assertEquals("9", recordAttributes.get(0).getValue());
-        assertEquals("Packets", recordAttributes.get(8).getName());
-        assertEquals("73", recordAttributes.get(8).getValue());
-        assertEquals("dstAS", recordAttributes.get(24).getName());
-        assertEquals("24", recordAttributes.get(24).getValue());
+        assertEquals(27, storedRecords.get(1).getRecordAttributes().size());
+        assertEquals("version", storedRecords.get(0).getRecordAttributes().get(0).getName());
+        assertEquals("5", storedRecords.get(0).getRecordAttributes().get(0).getValue());
+        assertEquals(SOURCE_IP, storedRecords.get(1).getNodeID());
+        assertEquals(Integer.valueOf(2), storedRecords.get(1).getIndex());
+        assertEquals(DataCategory.NETFLOW, storedRecords.get(1).getTSDRDataCategory());
+    }
 
-        recordAttributes = storedRecords.get(storedRecords.size() - 1).getRecordAttributes();
-        assertEquals(26, recordAttributes.size());
-        assertEquals("version", recordAttributes.get(0).getName());
-        assertEquals("9", recordAttributes.get(0).getValue());
-        assertEquals("Packets", recordAttributes.get(8).getName());
-        assertEquals("89", recordAttributes.get(8).getValue());
-        assertEquals("dstAS", recordAttributes.get(24).getName());
-        assertEquals("22", recordAttributes.get(24).getValue());
+    private byte[] generateV5Data() {
+        byte[] data = new byte[72];
+        data[1] = 5;  // version
+        data[3] = 1;  // flow count
+        return data;
     }
 }
